8689bee261d6af72ee5316c939f5df98531cb5a1
dela <princeradikal@yahoo.com>
2020-05-15 23:12:43 +0000

    Day 1: Text animation

--- 100-days-js/day-1/index.js
+++ 100-days-js/day-1/index.js
@@ -2,27 +2,60 @@
   const title = document.querySelector(".animatable");
   const subtitle = document.querySelector(".animate-slide");
 
-  const titleText = title.textContent.trim();
-  const subtitleText = subtitle.textContent.trim();
+  title.innerHTML = transformToSpans(splitUpContent(title));
+  subtitle.innerHTML = transformToSpans(splitUpContent(subtitle));
 
-  const subtitleSplit = subtitleText.split("");
+  let colorIndex = 0;
 
-  let subtitlehtml = "";
-  for (const elem of subtitleSplit) {
-    subtitlehtml += `<span>${elem}</span>`;
-  }
-
-  subtitle.innerHTML = subtitlehtml;
+  setInterval(() => {
+    colorize(title.querySelectorAll("span"), colorIndex);
+    colorIndex = (colorIndex + 1) % 10;
+  }, 2500);
 
   setTimeout(() => slideIn(subtitle.querySelectorAll("span")), 1000);
 })();
 
-function slideIn(elems) {
+function splitUpContent(domElement) {
+  let textContent = domElement.textContent.trim();
+  return textContent.split("");
+}
+
+function slideIn(spans) {
   time = 100;
-  for (const elem of elems) {
+  for (const span of spans) {
     setTimeout(() => {
-      elem.classList.add("slide-in");
+      span.classList.add("slide-in");
     }, time);
     time += 100;
+  }
+}
+
+function transformToSpans(stringArray) {
+  let htmlstring = "";
+  for (const str of stringArray) {
+    htmlstring += `<span>${str}</span>`;
+  }
+  return htmlstring;
+}
+
+function colorize(spans, colorIndex) {
+  const colors = [
+    "green",
+    "red",
+    "blue",
+    "orange",
+    "yellow",
+    "purple",
+    "brown",
+    "violet",
+    "white",
+    "pink",
+  ];
+
+  console.log(colorIndex);
+  let timer = 100;
+  for (let span of spans) {
+    setTimeout(() => (span.style.color = colors[colorIndex]), timer);
+    timer += 100;
   }
 }
