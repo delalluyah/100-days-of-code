a74c4e85a9c062619bf50d03f77b6599db57becf
delalluyah <princefold@gmail.com>
2020-12-26 17:08:35 +0000

    added animation to modal

--- 100-days-js/day-11/src/components/App/index.js
+++ 100-days-js/day-11/src/components/App/index.js
@@ -4,7 +4,11 @@ import Modal from "../Modal";
 function App() {
   return (
     <div className="main-content">
-      <Modal display_link={true}>
+      <Modal
+        display_link={true}
+        target_id="sample_modal1"
+        header="My awesome modal"
+      >
         <p style={{ textAlign: "center" }}> Hello there!</p>
       </Modal>
     </div>
--- 100-days-js/day-11/src/components/Modal/index.js
+++ 100-days-js/day-11/src/components/Modal/index.js
@@ -20,10 +20,23 @@ const Modal = ({
         onClick={() => {
           let modalOverlay = document.createElement("div");
           modalOverlay.classList.add("modal-overlay");
-          let modal = document.querySelector(`#${modal_id}`);
+          let modal = document.querySelector(`#${modal_id}`).cloneNode(true);
           modalOverlay.appendChild(modal);
           document.body.prepend(modalOverlay);
+          modal
+            .querySelector(".modal-close-btn")
+            .addEventListener("click", (e) => {
+              let targetModalOverlay = e.currentTarget.closest(
+                ".modal-overlay"
+              );
+              modal.classList.remove("show");
+              modal.classList.add("modal-hide");
+              setTimeout(() => {
+                targetModalOverlay.parentNode.removeChild(targetModalOverlay);
+              }, 400);
+            });
           modal.style.display = "block";
+          modal.classList.add("show");
         }}
       >
         Open Modal
@@ -33,7 +46,7 @@ const Modal = ({
           <p className="title">{header}</p>
           <a href="#!" className="modal-close-btn">
             {" "}
-            <img src={close_icon} />{" "}
+            <img alt="close" src={close_icon} />{" "}
           </a>
         </header>
         <section className="modal-body">{children}</section>
